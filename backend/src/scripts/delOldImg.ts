4a37b6e7364e8c74311e52b86455809e220e8c9d
Yuri <yur0n@yahoo.com>
2024-07-15 15:55:08 +0300

    up

--- backend/src/scripts/delOldImg.ts
+++ backend/src/scripts/delOldImg.ts
@@ -1,8 +1,6 @@
 import fs from 'fs';
 import path from 'path';
 
-console.log(process.cwd());
-
 function isFileOldEnough(filePath: string, threshold = 5 * 24 * 60 * 60 * 1000) { //every 5 days
   const fileStats = fs.statSync(filePath);
   const fileTime = fileStats.mtimeMs;
@@ -22,8 +20,8 @@ function deleteFile(filePath: string) {
 function cleanOldImages(directoryPath: string) {
   fs.readdirSync(directoryPath).forEach((fileName) => {
 		console.log(fileName)
-    const filePath = path.join(directoryPath, fileName); // Use path.join if needed
-    const isImage = (/\.(jpg|jpeg|png)$/i).test(fileName); // Check for common image extensions (case-insensitive)
+    const filePath = path.join(directoryPath, fileName);
+    const isImage = (/\.(jpg|jpeg|png)$/i).test(fileName);
     if (isImage && isFileOldEnough(filePath)) {
       deleteFile(filePath);
     }
@@ -31,8 +29,8 @@ function cleanOldImages(directoryPath: string) {
 }
 
 const imageDirectory = './public/a';
-
 const interval = 1 * 24 * 60 * 60 * 1000 //everyday
+
 setInterval(() => {
 	try {
 		cleanOldImages(imageDirectory);
